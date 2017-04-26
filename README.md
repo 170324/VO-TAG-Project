54e6de66faa35e0b88534b086098d4ca615ad6fd
rice LH <rice LH>
2017-04-25 21:20:17 -0400

    added more information

---
+++ README.md
@@ -0,0 +1,1 @@
+2016 - 2017 VO-TAG Project game based off of The Oregon Trail
--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import msvcrt
 
 class Player:
-	""" defines the base stats of the player """
-	
+	""" Player: defines the base stats of the player """
+
 	health = 100
-	
+
 	def __init__(self, name):
 		self.name = name
-		
+
+class Item:
+	""" Will be a template for making items such as medicine or food """
+
+	def __init__(self):
+		pass	# will require information about what item it is
+
+class Inventory:
+	""" Inventory: will maintain the current state of the player's items """
+
+	def __init__(self):
+		self.items = {}
+
+	def __str__(self):
+		pass	# will print your inventory to the player
+
+	def add_item():
+		pass	# will add an item to the player's invetory
+
 def get_names():
 	""" get_names: gets names of players and returns a list """
-	
+
 	i = 0
 	names = []
 	while (i < 3):
@@ -21,7 +39,7 @@ def get_names():
 		names.append(name)
 		i += 1
 	return names
-	
+
 print("Hello, welcome to A Journey To Mars. [Press enter to continue]")
 msvcrt.getch()
 print("Give me the names of you and the other two members of your crew:")
