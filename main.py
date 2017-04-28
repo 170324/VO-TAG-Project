b7f2e5e41030f2c5bfa2a3918f70c0a3fe2dc80b
170233 <170233@bcit.cc>
2017-04-28 12:34:24 -0400

    added in items and diseases

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Player:
 
 	health = 100
 	sick = False
-	morale = "standard"		# three types of morale, [low, standard, and high]
+	morale = "standard"				# three types of morale, [low, standard, and high]
 							# it matters when a problem occurs, if a player has
 							# low morale than it is harder to fix something,
 							# the opposite as if he had high morale
@@ -14,21 +14,20 @@ class Player:
 		self.name = name
 
 class Item:
-	""" Will be a template for making items such as medicine or food """
+	""" Item: will be a template for making items such as medicine or food """
 
-	def __init__(self, name, pounds, heal=0):
+	def __init__(self, name, pounds, heal=False):
 		self.name = name
 		self.pounds = pounds
-		self.heal = heal
+		self.heal = heal # will be True if it can get rid of sickness
 
 class Problem:
-	""" Will consist of all of the diseases, spaceship problems, and 'space madness' values """
+	""" Problem: will consist of all of the diseases, spaceship problems, and 'space madness' values """
 	
-	def __init__(self, name, chance, death_timer, recovery):
-		self.name = name					# name of problem
-		self.chance = chance				# chance to get it
-		self.death_timer = death_timer		# turns it takes to die with it
-		self.recovery = recovery			# chance to get rid of it
+	def __init__(self, name, altname, death_timer, recovery):
+		self.name = name							# name of problem
+		self.death_timer = death_timer						# turns it takes to die with it
+		self.recovery = percentage(recovery, 100)				# chance to get rid of it
 
 class Inventory:
 	""" Inventory: will maintain the current state of the player's items """
@@ -56,6 +55,11 @@ def get_names():
 		i += 1
 	return names
 
+def percentage(part, whole):
+	""" percentage: turn a regular number into a percentage """
+	
+	return 100 * float(part)/float(whole)
+
 day = 0			# number day it is, 26 turns in total
 
 print("Hello, welcome to A Journey To Mars. [Press enter to continue]")
@@ -65,6 +69,16 @@ names = get_names()
 print("The next step in the journey is that you must gather supplies.")
 msvcrt.getch()
 
+# definitions after inventory, will probably move into seperate file
+
 Player1 = Player(names[0])
 Player2 = Player(names[1])
 Player3 = Player(names[2])
+Flu = Problem("Flu", "the flu", 4, 30)
+S_virus = Problem("Stomach Virus", "a stomach virus", 2, 80)
+Smallpox = Problem("Smallpox", "smallpox", 6, 6)
+Sprain = Problem("Sprain", "a sprain", 15, 90)
+B_arm = Problem("Broken Arm", "a broken arm", 0, 50)
+Food = Item("Food", 40)
+Meds = Item("Medicine", 50, True)
+S_parts = Item("Spare Parts", 400)
